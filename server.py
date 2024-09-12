85ff9fb4b7885536446ddadec3c1f6fd0f88cc72
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-12 15:55:39 +0800

    run some linting

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 app = Flask("Emotion Detector")
 
+
 @app.route("/emotionDetector")
 def sent_detector():
     # Retrieve the text to analyze from the request arguments
@@ -9,22 +10,18 @@ def sent_detector():
 
     # Pass the text to the emotion_detector function and store the response
     emotions = emotion_detector(text_to_analyze)
-    
+
     if emotions['dominant_emotion'] is None:
         return 'Invalid text! Please try again!'
 
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(
-        emotions['anger'],
-        emotions['disgust'],
-        emotions['fear'],
-        emotions['joy'],
-        emotions['sadness'],
-        emotions['dominant_emotion']
-    )
+        emotions['anger'], emotions['disgust'], emotions['fear'], emotions['joy'], emotions['sadness'], emotions['dominant_emotion'])
+
 
 @app.route("/")
 def render_index_page():
     return render_template('index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
