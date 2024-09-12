f62a0ba26b0784ea7990beea65a122cf422270e1
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-12 14:04:12 +0800

    format emotions json output task3

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,19 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj =  { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response.emotionPredictions[0].emotion
+    
+    max_score = -1
+    dominant_emotion = ""
+    for key, value in emotions:
+        if value > max_score:
+            max_score = value
+            dominant_emotion = key
+    emotions['dominant_emotion'] = dominant_emotion
+    return emotions
