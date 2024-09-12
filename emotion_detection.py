fcfd173c063dcf3435aa4a1b3046a1d1a15b8c54
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-12 14:13:20 +0800

    fix bug

--- emotion_detection.py
+++ emotion_detection.py
@@ -7,11 +7,12 @@ def emotion_detector(text_to_analyze):
     myobj =  { "raw_document": { "text": text_to_analyze } }
     response = requests.post(url, json = myobj, headers=headers)
     formatted_response = json.loads(response.text)
-    emotions = formatted_response.emotionPredictions[0].emotion
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
     
     max_score = -1
     dominant_emotion = ""
-    for key, value in emotions:
+    for key in emotions:
+        value = emotions[key]
         if value > max_score:
             max_score = value
             dominant_emotion = key
