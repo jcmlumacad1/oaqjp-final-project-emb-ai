6a18912ce297bb6a2d9b4de6f4404e744f0cabd1
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-12 15:19:03 +0800

    deploy using flask task6

---
+++ server.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_detector():
+    # Retrieve the text to analyze from the request arguments
+    text_to_analyze = request.args.get('textToAnalyze')
+
+    # Pass the text to the emotion_detector function and store the response
+    emotions = emotion_detector(text_to_analyze)
+
+    return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(
+        emotions['anger'],
+        emotions['disgust'],
+        emotions['fear'],
+        emotions['joy'],
+        emotions['sadness'],
+        emotions['dominant_emotion']
+    )
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
