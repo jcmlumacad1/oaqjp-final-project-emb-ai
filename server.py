b04244db5a9d6512efde934bff28beeb3a417374
jcmlumacad1 <44424424+jcmlumacad1@users.noreply.github.com>
2024-09-12 16:21:43 +0800

    save

--- server.py
+++ server.py
@@ -1,14 +1,5 @@
 """
 Emotion Detector Flask Application
-
-This Flask application provides an emotion detection service. It uses an emotion detection model 
-to analyze a given text input and return the emotions detected, including the dominant emotion, 
-if any. It includes two routes: one for rendering the index page and another for performing 
-emotion detection.
-
-The application depends on an external `emotion_detector` function from the 
-`EmotionDetection.emotion_detection` module, which processes the text and returns the associated 
-emotions.
 """
 
 from flask import Flask, render_template, request
@@ -20,15 +11,6 @@ app = Flask("Emotion Detector")
 def sent_detector():
     """
     Detect emotions from a given text input.
-
-    Retrieves the text input from the request arguments, passes it to the `emotion_detector` 
-    function, and returns a response indicating the detected emotions. If the `dominant_emotion` 
-    is None, an error message is returned.
-
-    Returns:
-        str: A string displaying the detected emotions ('anger', 'disgust', 'fear', 'joy', 
-        'sadness') and the dominant emotion. If no valid text is provided or no dominant emotion 
-        is detected, an error message is returned.
     """
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
@@ -54,11 +36,6 @@ def sent_detector():
 def render_index_page():
     """
     Render the index page.
-
-    Renders and returns the content of the `index.html` template when the root URL is accessed.
-
-    Returns:
-        str: The rendered content of the index page.
     """
     return render_template('index.html')
 
